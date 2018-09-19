32e8b1812864e92dfd4048af69e700f1c99aecc9
ingcomas <sebacomas>
2018-09-19 16:32:23 -0300

    Twitter!

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const tweetBank = require('../tweetBank');
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true } );
 });
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
