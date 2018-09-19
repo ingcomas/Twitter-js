34506bad123735f3fe68827d6f4673674994d92b
ingcomas <sebacomas>
2018-09-19 10:35:20 -0300

    Twitter!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
 
 app.use(function(req,res,next){// logging Middlewere de url y método
     console.log('url: '+req.url,'\tMetodo: '+req.method);
