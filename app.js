0cbd72fea81a464188fda2ee49bdd3bdc4fc0d5c
ingcomas <sebacomas>
2018-09-19 10:20:37 -0300

    Twitter!

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(function(req,res,next){// logging Middlewere de url y método
+    console.log('url: '+req.url,'\tMetodo: '+req.method);
+    next();
+});
+app.use('/special*',function(req,res,next){
+    console.log('Accediste a un lugar especial');
+    res.send('<h1>Es un lugar muuuuuy especial</h1> <br> <h3>si no me creés, preguntale a franche</h3> <br> <a href="https://www.youtube.com/watch?v=NgALKLCswi0"> Mirá este video</a>');
+
+    next();
+})
+
+app.listen('3000');// Espera un request por puerto 3000
